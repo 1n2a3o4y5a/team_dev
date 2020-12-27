c6af54af0cef20599575399b02cd5c2e8e007cd6
岸直哉 <user.id.125125@gmail.com>
2020-12-27 09:52:24 +0900

    店名テーブル作成途中

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Shops(models.Model):
+    class Meta:
+        db_table = 'shops'
+        verbose_name = 'shops'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='店名')
+    address_prefecture = models.CharField(verbose_name='都道府県名')
+    adress_city = models.CharField(verbose_name='市区町村名')
+    adress_detail = models.CharField(verbose_name='住所詳細')
+    nearest_station = models.CharField(verbose_name='最寄駅')
+    about_time_walk = models.CharField(verbose_name='駅徒歩時間')
+    review = models.IntegerField(vervose_name='レビュー')
+    
+    
