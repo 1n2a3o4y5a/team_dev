e9f049450d385a6fb27b3c18b49f43ccddee46a7
岸直哉 <user.id.125125@gmail.com>
2020-12-30 15:50:19 +0900

    住所マスタ作成途中

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -1,6 +1,37 @@
 from django.db import models
 from django.utils import timezone
 
+
+class MasterRegions:
+    class Meta:
+        db_name = 'm_regions'
+        verbose_name = '地方マスタ'
+    
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='地方名')
+
+
+class MasterPrefectures:
+    class Meta:
+        db_name = 'm_prefectures'
+        verbose_name = '都道府県マスタ'
+
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    region_id = models.ForeignKey(MasterRegions, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='都道府県名')
+    
+
+class MasterCities:
+    class Meta:
+        db_name = 'm_cities'
+        verbose_name = '市区町村マスタ'
+    
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    region_id = models.ForeignKey(MasterRegions, on_delete=models.CASCADE)
+    prefecture_id = models.ForeignKey(MasterPrefectures, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='市区町村名')
+
+
 class Shops(models.Model):
     class Meta:
         db_table = 'shops'
@@ -8,11 +39,3 @@ class Shops(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(verbose_name='店名')
-    address_prefecture = models.CharField(verbose_name='都道府県名')
-    adress_city = models.CharField(verbose_name='市区町村名')
-    adress_detail = models.CharField(verbose_name='住所詳細')
-    nearest_station = models.CharField(verbose_name='最寄駅')
-    about_time_walk = models.CharField(verbose_name='駅徒歩時間')
-    review = models.IntegerField(vervose_name='レビュー')
-    
-    
