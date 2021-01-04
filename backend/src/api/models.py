d1dcf09d0fa11315ef12598937e9ac9edb876131
岸直哉 <user.id.125125@gmail.com>
2021-01-04 16:10:15 +0900

    マイグレーションファイル作成途中

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -19,8 +19,8 @@ class MasterPrefectures:
 
     prefecture_code = models.UUIDField(default=uuid.uuid4, editable=False)
     # region_id = models.ForeignKey(MasterRegions, on_delete=models.CASCADE)
-    prefecture_name = models.CharField(verbose_name='都道府県名')
-    prefecture_name_ruby = models.CharField(verbose_name='都道府県名カナ')
+    prefecture_name = models.CharField(verbose_name='都道府県名', max_length=20)
+    prefecture_name_ruby = models.CharField(verbose_name='都道府県名カナ', max_length=50)
 
     
 
@@ -31,9 +31,9 @@ class MasterCities:
     
     city_code = models.UUIDField(default=uuid.uuid4, editable=False)
     # region_id = models.ForeignKey(MasterRegions, on_delete=models.CASCADE)
-    prefecture_code = models.ForeignKey(MasterPrefectures, on_delete=models.CASCADE)
-    city_name = models.CharField(verbose_name='市区町村名')
-    city_name_ruby = models.CharField(verbose_name='市区町村名カナ')
+    prefecture_code = models.ForeignKey('MasterPrefectures', to_field='prefecture_code', on_delete=models.CASCADE)
+    city_name = models.CharField(verbose_name='市区町村名', max_length=20)
+    city_name_ruby = models.CharField(verbose_name='市区町村名カナ', max_length=50)
 
 
 class Shops(models.Model):
@@ -42,7 +42,7 @@ class Shops(models.Model):
         verbose_name = 'shops'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(verbose_name='店名')
-    adress_prefecture = models.ForeignKey(MasterPrefectures, on_delete=models.CASCADE)
-    adress_city = models.ForeignKey(MasterCities, on_delete=models.CASCADE)
-    adress_detail = models.CharField(verbose_name='住所詳細')
+    name = models.CharField(verbose_name='店名', max_length=50)
+    adress_prefecture = models.ForeignKey('MasterPrefectures', to_field='prefecture_code', on_delete=models.CASCADE)
+    adress_city = models.ForeignKey('MasterCities', to_field='city_code', on_delete=models.CASCADE)
+    adress_detail = models.CharField(verbose_name='住所詳細', max_length=50)
