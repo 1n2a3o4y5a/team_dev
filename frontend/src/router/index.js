8d92a41b62e8f70e05b89cb83dcd5436f8829213
岸直哉 <user.id.125125@gmail.com>
2021-01-17 15:26:10 +0900

    router修正

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import MainLayout from '../component/rayout/MainLayout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'mainlayout',
+    component: MainLayout,
+    // children: [
+    //   {
+    //     path: 'shops',
+    //     name: 'shops',
+    //     component: ShopIndex
+    //   },
+    //   {
+    //     path: 'postShop',
+    //     path: 'postShop',
+    //     component: postShopIndex
+    //   }
+    // ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
