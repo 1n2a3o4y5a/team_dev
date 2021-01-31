6295fdce55883933fcb0b6527384979cb34946d0
岸直哉 <user.id.125125@gmail.com>
2021-01-31 18:32:24 +0900

    fixtureエラー

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -9,7 +9,7 @@ class MasterPrefecture(models.Model):
         db_table = 'm_prefecture'
         verbose_name = '都道府県マスタ'
 
-    prefecture_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    prefecture_code = models.IntegerField(primary_key=True,editable=False, unique=True)
     prefecture_name = models.CharField(verbose_name='都道府県名', max_length=20)
     prefecture_name_ruby = models.CharField(verbose_name='都道府県名カナ', max_length=50)
 
@@ -20,18 +20,18 @@ class MasterCity(models.Model):
         db_table = 'm_city'
         verbose_name = '市区町村マスタ'
     
-    city_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    city_code = models.IntegerField(primary_key=True, editable=False, unique=True)
     prefecture_code = models.ForeignKey('MasterPrefecture', to_field='prefecture_code', on_delete=models.CASCADE)
     city_name = models.CharField(verbose_name='市区町村名', max_length=20)
     city_name_ruby = models.CharField(verbose_name='市区町村名カナ', max_length=50)
 
 
-class Shops(models.Model):
+class Shop(models.Model):
     class Meta:
         db_table = 'shop'
-        verbose_name = 'shop'
+        verbose_name = '店'
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.IntegerField(primary_key=True, editable=False, unique=True)
     name = models.CharField(verbose_name='店名', max_length=100)
     adress_prefecture = models.ForeignKey('MasterPrefecture', to_field='prefecture_code', on_delete=models.CASCADE)
     adress_city = models.ForeignKey('MasterCity', to_field='city_code', on_delete=models.CASCADE)
