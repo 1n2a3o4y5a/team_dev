6fe095a14c5de5394185d4e62e9de7a75cc5a547
岸直哉 <user.id.125125@gmail.com>
2021-02-11 19:44:05 +0900

    model変更

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -28,7 +28,7 @@ class Shop(models.Model):
     class Meta:
         db_table = 'shop'
 
-    id = models.IntegerField(primary_key=True, editable=False, unique=True)
+    id = models.AutoField(primary_key=True, editable=False, unique=True)
     name = models.CharField(max_length=100)
     adress_prefecture = models.ForeignKey('MasterPrefecture', to_field='prefecture_code', on_delete=models.CASCADE)
     adress_city = models.ForeignKey('MasterCity', to_field='city_code', on_delete=models.CASCADE)
