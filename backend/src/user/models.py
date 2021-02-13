21428d55aff9a13be18aea64335aca517707835c
岸直哉 <user.id.125125@gmail.com>
2021-02-13 19:10:29 +0900

    userid追加

--- backend/src/user/models.py
+++ backend/src/user/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True, editable=False, unique=True)
