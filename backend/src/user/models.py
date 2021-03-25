816e02c16b138bce9a8de0e2554276dc868e26ae
岸直哉 <user.id.125125@gmail.com>
2021-03-25 23:06:44 +0900

    laravelに変更途中

--- backend/src/user/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    id = models.AutoField(primary_key=True, editable=False, unique=True)
